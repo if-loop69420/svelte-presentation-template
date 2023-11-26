5b031ac288461e90dcce9e9c65dcd523fbd8dd7a
if-loop69420 <j.sztavi@pm.me>
2023-11-26 19:33:51 +0100

    Initial commit

--- .svelte-kit/generated/client/nodes/1.js
+++ .svelte-kit/generated/client/nodes/1.js
@@ -1,1 +1,1 @@
-export { default as component } from "../../../../node_modules/.pnpm/@sveltejs+kit@1.25.0_svelte@4.2.1+vite@4.4.9/node_modules/@sveltejs/kit/src/runtime/components/error.svelte";
+export { default as component } from "../../../../node_modules/@sveltejs/kit/src/runtime/components/error.svelte";
--- src/lib/deck/presentation.svelte
+++ src/lib/deck/presentation.svelte
@@ -3,3 +3,49 @@
 	import Code from './code.svelte'
 	import Notes from './notes.svelte'
 </script>
+
+<Slide animate id="intro">
+	<Slide animate>
+		<h1>T-SQL vs. PL/SQL</h1>
+		<p>Presentation by Jeremy Sztavinovszki & Moritz Skrepek</p>
+	</Slide>
+
+	<Slide animate>
+		<h3>Table Of Contents</h3>
+	</Slide>
+
+	<Slide animate>
+		<h3>Table Of Contents</h3>
+		<ol>
+			<li>
+				<p>Syntax</p>
+				<ol> 
+					<li>Syntax T-SQL</li>
+					<li>Syntax PL/SQL</li>
+				</ol>
+			</li>
+		</ol>
+	</Slide>
+
+	<Slide animate>
+		<h3>Table Of Contents</h3>
+		<ol>
+			<li>
+				<p>Syntax</p>
+				<ol> 
+					<li>Syntax T-SQL</li>
+					<li>Syntax PL/SQL</li>
+				</ol>
+			</li>
+			<li>
+				<p>Usage Möglichkeiten</p>
+				<ol>
+					<li>Mutating Triggers</li>
+					<li>Reihenfolge</li>
+				</ol>
+			</li>
+		</ol>
+	</Slide>
+	
+</Slide>
+
